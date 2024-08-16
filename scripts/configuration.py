2768254a26b78280f61f023efd6f220994020357
DenyaOmega3 <rpoj18.1.denya.lobanov1@gmail.com>
2024-08-16 10:53:04 +0300

    comment, remove magic strings

--- scripts/configuration.py
+++ scripts/configuration.py
@@ -1,1 +1,9 @@
 JSON_FILENAME = "../json/Python-task.json"
+
+# json_keys
+ASSIGNMENT_RESULTS = 'assignment_results'
+SHOWN_PRICE = 'shown_price'
+NUMBER_OF_GUESTS = 'number_of_guests'
+EXT_DATA = 'ext_data'
+TAXES = 'taxes'
+NET_PRICE = 'net_price'
--- scripts/functions.py
+++ scripts/functions.py
@@ -1,16 +1,20 @@
 import json
 from decimal import Decimal
+from configuration import *
 
 def load_json_file(path):
+    """ Reads a specific json file """
+
     with open(path) as json_file:
         json_data = json.load(json_file)
     return json_data
 
 def get_the_cheapest_price(json_string):
-    shown_price = json_string['assignment_results'][0]['shown_price']
+    """ Finds the cheapest price from given json (without using min function) """
+
+    shown_price = json_string[ASSIGNMENT_RESULTS][0][SHOWN_PRICE]
     min_price = shown_price[next(iter(shown_price))]
 
-    # an implementation how to find min without using min()
     for price in shown_price:
         if Decimal(min_price) > Decimal(shown_price[price]):
             min_price = shown_price[price]
@@ -18,11 +22,14 @@ def get_the_cheapest_price(json_string):
     return min_price
 
 def get_number_of_guests(json_string):
+    """ Returns the number of guests """
     # the number of guests does not depend on a price we give
-    return json_string['assignment_results'][0]['number_of_guests']
+    return json_string[ASSIGNMENT_RESULTS][0][NUMBER_OF_GUESTS]
 
 def get_room_type_by_price(json_string, given_price):
-    shown_price = json_string['assignment_results'][0]['shown_price']
+    """ Returns room types by price based on the price """
+
+    shown_price = json_string[ASSIGNMENT_RESULTS][0][SHOWN_PRICE]
     rooms = [] # there might be several rooms with the same price
     for room_type, price in shown_price.items():
         if price == given_price:
@@ -31,7 +38,9 @@ def get_room_type_by_price(json_string, given_price):
     return rooms
 
 def get_total_tax(json_string):
-    taxes = json_string['assignment_results'][0]['ext_data']['taxes']  # a string that contains .json
+    """ Evaluates total tax price """
+
+    taxes = json_string[ASSIGNMENT_RESULTS][0][EXT_DATA][TAXES]  # a string that contains .json
     taxes = json.loads(taxes)  # convert into .json
     sum_of_taxes = 0
     for tax in taxes:
@@ -40,7 +49,9 @@ def get_total_tax(json_string):
     return sum_of_taxes
 
 def get_total_price_for_each_room(json_string):
-    net_price = json_string['assignment_results'][0]['net_price']
+    """ Returns total price (including tax price) for each room """
+
+    net_price = json_string[ASSIGNMENT_RESULTS][0][NET_PRICE]
     total_tax = get_total_tax(json_string)
     for price in net_price:
         net_price[price] = str(Decimal(net_price[price]) + total_tax)
